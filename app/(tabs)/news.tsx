dcf5401e358d561e493e7d65379a48e2c0c882fb
Thomas Tsoi <tsoithomas@outlook.com>
2024-11-17 18:22:49 -0500

    Getting URL from webView

--- app/(tabs)/news.tsx
+++ app/(tabs)/news.tsx
@@ -2,15 +2,21 @@ import { StyleSheet } from 'react-native';
 
 import { Text, View } from '@/components/Themed';
 import HeaderSemiCircle from '@/components/HeaderSemiCircle';
-import WebView from 'react-native-webview';
+import WebView, { WebViewMessageEvent } from 'react-native-webview';
 
 export default function NewsScreen() {
+	const handleMessage = (event: WebViewMessageEvent) => {
+		const message = event.nativeEvent.data;
+		console.log('Message from WebView:', message);
+	};
+
   return (
 	<View style={styles.container}>
 		<HeaderSemiCircle  />
 		<WebView
 			style={styles.webView}
 			source={{ uri: 'https://www.ktls.edu.hk/news/app/' }}
+			onMessage={handleMessage}
 		/>
 
 	</View>
